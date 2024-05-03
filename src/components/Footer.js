fa87b149f17db47d2ca3762aa433564ee9a175b5
Antonio Zampino <zampino.antonio.97@gmail.com>
2024-05-03 22:33:23 +0200

    content scroll fix + links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,19 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
+
+    const redirectGitHub = () => {
+        window.open("https://github.com/ant-zmp");
+    }
+
+    const redirectLinkedIn = () => {
+        window.open("https://www.linkedin.com/in/okzampo/");
+    }
+
     return (
         <div className={"footer"}>
             <div className={"socialMedia"}>
-                <GitHubIcon/> <LinkedInIcon/>
+                <GitHubIcon onClick={redirectGitHub}/> <LinkedInIcon onClick={redirectLinkedIn}/>
             </div>
             <p>&copy; 2024 - awesomepizza.com </p>
         </div>
